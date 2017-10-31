cbba3d714d943804c2765c3a8548e85a9d3d18a9
Braden Hitchcock <hitchcock.braden@gmail.com>
2017-10-31 15:27:53 -0600

    Implemented definitions file and data folder

--- src/VariantInterpretationAnalysis/gene-via.py
+++ src/VariantInterpretationAnalysis/gene-via.py
@@ -5,5 +5,9 @@
 #
 
 # Import required files
+from VariantInterpretationAnalysis.Initializer import Initializer
+from VariantInterpretationAnalysis.Definitions import DATA_DIR
 
 # Start program
+mutationsFile = DATA_DIR + "mutations.txt"
+wekaData = Initializer.initWekaData(mutationsFile)
