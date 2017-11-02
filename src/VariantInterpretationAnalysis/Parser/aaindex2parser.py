169e57206774eacaf2fb8be4a22c5a57d9888dbc
Kaleb White <kaleb.b.white@hpe.com>
2017-11-02 13:37:21 -0600

    1

--- src/VariantInterpretationAnalysis/Parser/aaindex2parser.py
+++ src/VariantInterpretationAnalysis/Parser/aaindex2parser.py
@@ -1,59 +1,44 @@
 import sys
+import re
 sys.path.append('../..')
 from VariantInterpretationAnalysis.Definitions import DATA_DIR
 #######
-#aaindex2parser.py old new group
-#
-#Where old is the old acid, new is what the acid changed into.
-#Group is what group you want (0-99)
+#aaindex2parser.py
 ######
-
-#static String[] amnio_acids = {"Ala", "Arg", "Asn", "Asp", "Cys", "Gln", "Glu", "Gly", "His", "Ile", "Leu", "Lys", "Met", "Phe", "Pro", "Ser", "Thr", "Trp", "Tyr", "Val", "Ter"};
-#static String acid_abbreviations = "ARNDCQEGHILKMFPSTWYV*";
-
-#old,new,and group are 0 indexed
+map={}
 acid_abr = "ARNDCQEGHILKMFPSTWYV*"
-
-old=int(acid_abr.index(sys.argv[1]))
-new=int(acid_abr.index(sys.argv[2]))
-#group=int(acid_abr.index(sys.argv[3]))
-#old=int(sys.argv[1]) # eg 0
-#new=int(sys.argv[2]) # eg 1
-group=int(sys.argv[3]) # eg 4
-
-#print( old )
-#print( new )
-#print( group )
 #######
-min=min(old,new)
-max=max(old,new)
 group_counter=-1
 row_counter=0
 with open(DATA_DIR+"aaindex2") as f:
     for line in f:
         #print(line)
-        if group_counter > -1:
-            #print(line)
-            if row_counter == max:
-                #print(line)
-                items = line.split("    ")
-                print(items[min+1].strip())
-            row_counter = row_counter + 1
-        if "M rows = ARNDCQEGHILKMFPSTWYV" in line:
+        if "M rows = " in line:
+            start = line.index("M rows =")+9
+            end = line.index(",")
+            acid_abr = line[start:end]
+            #print("THIS" + acid_abr)
             group_counter = group_counter + 1
             row_counter = 0
-
-#with open(DATA_DIR+"aaindex2") as f:
-#    for line in f:
-#        #print(line)
-##        if group_counter == group:
-#           #print(line)
-#            if row_counter == max:
-#                #print(line)
-#                items = line.split("    ")
-#                print(items[min+1].strip())
-#            row_counter = row_counter + 1
-#        if "M rows = ARNDCQEGHILKMFPSTWYV" in line:
-#            group_counter = group_counter + 1
-#            row_counter = 0
-        
+        if group_counter > -1:
+            if re.search('[a-zA-Z#]', line) or line.strip() == "": 
+                ## Contains uppercase letters
+                continue
+            #print(line)
+            items = line.split()
+            if len(items) < 2:
+                continue
+            #print("ROW "+line)
+            for item in range(len(items)):
+                if item == 0:
+                    continue
+                col = item-1
+                #print(str(col)+" r:"+str(row_counter)+" g:"+str(group_counter))
+                
+                forward = str(group_counter)+""+acid_abr[col]+" "+acid_abr[row_counter]
+                backward = str(group_counter)+""+acid_abr[row_counter]+" "+acid_abr[col]
+                
+                map[forward] = str(items[item].strip())
+                map[backward] = str(items[item].strip())
+                print(str(group_counter)+" "+forward+ ": "+str(items[item].strip()))
+            row_counter = row_counter + 1
