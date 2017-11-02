f692ec04228f1f69943d3e1eb8f3afb110cb4d81
Kaleb White <kaleb.b.white@hpe.com>
2017-11-02 12:55:01 -0600

    add

--- src/VariantInterpretationAnalysis/Parser/aaindex2parser.py
+++ src/VariantInterpretationAnalysis/Parser/aaindex2parser.py
@@ -1,4 +1,6 @@
 import sys
+sys.path.append('../..')
+from VariantInterpretationAnalysis.Definitions import DATA_DIR
 #######
 #aaindex2parser.py old new group
 #
@@ -6,23 +8,31 @@ import sys
 #Group is what group you want (0-99)
 ######
 
-old=int(sys.argv[1]) # eg 0
-new=int(sys.argv[2]) # eg 1
+#static String[] amnio_acids = {"Ala", "Arg", "Asn", "Asp", "Cys", "Gln", "Glu", "Gly", "His", "Ile", "Leu", "Lys", "Met", "Phe", "Pro", "Ser", "Thr", "Trp", "Tyr", "Val", "Ter"};
+#static String acid_abbreviations = "ARNDCQEGHILKMFPSTWYV*";
+
+#old,new,and group are 0 indexed
+acid_abr = "ARNDCQEGHILKMFPSTWYV*"
+
+old=int(acid_abr.index(sys.argv[1]))
+new=int(acid_abr.index(sys.argv[2]))
+#group=int(acid_abr.index(sys.argv[3]))
+#old=int(sys.argv[1]) # eg 0
+#new=int(sys.argv[2]) # eg 1
 group=int(sys.argv[3]) # eg 4
 
 #print( old )
 #print( new )
 #print( group )
 #######
-acid_abbreviations = "ARNDCQEGHILKMFPSTWYV*"
 min=min(old,new)
 max=max(old,new)
 group_counter=-1
-row_counter=0;
-with open("C:\\aaindex2") as f:
+row_counter=0
+with open(DATA_DIR+"aaindex2") as f:
     for line in f:
         #print(line)
-        if group_counter == group:
+        if group_counter > -1:
             #print(line)
             if row_counter == max:
                 #print(line)
@@ -31,4 +41,19 @@ with open("C:\\aaindex2") as f:
             row_counter = row_counter + 1
         if "M rows = ARNDCQEGHILKMFPSTWYV" in line:
             group_counter = group_counter + 1
+            row_counter = 0
+
+#with open(DATA_DIR+"aaindex2") as f:
+#    for line in f:
+#        #print(line)
+##        if group_counter == group:
+#           #print(line)
+#            if row_counter == max:
+#                #print(line)
+#                items = line.split("    ")
+#                print(items[min+1].strip())
+#            row_counter = row_counter + 1
+#        if "M rows = ARNDCQEGHILKMFPSTWYV" in line:
+#            group_counter = group_counter + 1
+#            row_counter = 0
         
