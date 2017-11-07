f8314c2f8fd7dbf5c6189e2eba21ed6cb7dedd9d
Braden Hitchcock <hitchcock.braden@gmail.com>
2017-11-07 11:56:56 -0700

    Saving

--- src/VariantInterpretationAnalysis/Definitions.py
+++ src/VariantInterpretationAnalysis/Definitions.py
@@ -29,34 +29,4 @@ AVAILABLE_ALGORITHMS = [
 
 DEFAULT_ALGORITHMS = AVAILABLE_ALGORITHMS
 
-MUTATION_SYMBOLS_2_3 = [
-Ala     A       Alanine
-        Arg     R       Arginine
-        Asn     N       Asparagine
-        Asp     D       Aspartic acid (Aspartate)
-        Cys     C       Cysteine
-        Gln     Q       Glutamine
-        Glu     E       Glutamic acid (Glutamate)
-        Gly     G       Glycine
-        His     H       Histidine
-        Ile     I       Isoleucine
-        Leu     L       Leucine
-        Lys     K       Lysine
-        Met     M       Methionine
-        Phe     F       Phenylalanine
-        Pro     P       Proline
-        Ser     S       Serine
-        Thr     T       Threonine
-        Trp     W       Tryptophan
-        Tyr     Y       Tyrosine
-        Val     V       Valine
-        Asx     B       Aspartic acid or Asparagine
-        Glx     Z       Glutamine or Glutamic acid.
-        Xaa     X       Any amino acid.
-        TERM            termination codon
 
-]
-
-MUTATION_SYMBOLS_3_2 = [
-
-]
