e3bdc26343b319f8bc7fcecb504f0c5c80e32f1e
Kaleb White <kaleb.b.white@hpe.com>
2017-11-09 13:20:51 -0700

    addDefault

--- src/VariantInterpretationAnalysis/Initializer.py
+++ src/VariantInterpretationAnalysis/Initializer.py
@@ -7,7 +7,13 @@ import os
 
 
 from Collections import WekaData,Mutation
+from aaindex2parser import parse
 
+def getAAIndex2Map(data):
+    (default_feature_map, default_features) = parse() #calls aaindex2parser
+    data.__defaultFeatures = default_features
+    data.__defaultFeatureMap = default_feature_map
+    return
 
 # ------------------------------------------------------------------------------
 # Initializes a WekaData object with the names of mutations to be used in this
@@ -16,6 +22,7 @@ from Collections import WekaData,Mutation
 def initWekaData(filename):
     if os.path.exists(filename):
         data = WekaData()
+        getAAIndex2Map(data)
         with open(filename,"r") as mutationFile:
             for line in mutationFile:
                 data = line.strip().split()
@@ -26,5 +33,3 @@ def initWekaData(filename):
         return data
     else:
         return None
-
-
